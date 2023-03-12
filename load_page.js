5f279feacfca57c2bffaa69242564d1208696ba7
Timaxacker <laztimofey@gmail.com>
2023-03-12 17:20:58 +0300

    add form & table

---
+++ load_page.js
@@ -0,0 +1,12 @@
+let before_load_time = new Date().getTime();
+
+window.onload = page_load_time;
+	
+function page_load_time() {
+
+	document.getElementById("load_time").innerHTML = 
+		"Page load time is " + (new Date().getTime() - before_load_time) / 1000 + "</b> seconds";
+
+let s = "Page load time is " + (new Date().getTime() - before_load_time) / 1000 + "</b> seconds";
+	console.log(s)
+}
